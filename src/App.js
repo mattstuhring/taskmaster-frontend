159f28b8c80b154e941601f914386ad93a7aaac2
Matt Stuhring <whereyouatmatt@gmail.com>
2019-09-27 23:43:48 -0700

    Completed lab 39

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, {useState, useEffect} from 'react';
-import { Container, Row, Col, Card, Accordion, Button, Navbar, Modal } from 'react-bootstrap';
+import { Container, Row, Col, Card, Accordion, Button, Navbar } from 'react-bootstrap';
 
+import NewImageForm from './NewImageForm.js';
 import NewTaskForm from './NewTaskForm.js';
+import UpdateTaskAssignee from './UpdateTaskAssignee.js';
+import SubscribeForm from './SubscribeForm.js';
 import './App.scss';
 
 
-const API = 'http://taskmaster-dev-1.us-west-2.elasticbeanstalk.com/api/v1/tasks';
-
-
-// On Load - Get that data from the API
-// Iterate and display major task points
-// Some Interaction to expose history
-
-// TODO : Add a link to delete or change status
+//const API = 'http://taskmaster-dev.us-west-2.elasticbeanstalk.com/api/v1/tasks';
+const API = 'https://eic7g8klvd.execute-api.us-west-2.amazonaws.com/dev/tasks';
 
 function App() {
 
   const [tasks, setTasks] = useState([]);
 
-  function _getTasks() {
+  function handleGetTasks() {
     fetch(API)
       .then( data => data.json() )
       .then( fetchedTasks => setTasks(fetchedTasks) );
   }
 
-  useEffect( _getTasks, [] );
+  function handleDeleteTask(event, taskId) {
+    event.preventDefault();
+
+    const payload = {
+      id: taskId
+    };
+
+    fetch(API, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+      .then(res => res.json())
+      .then(() => handleGetTasks())
+      .catch(err => console.log(err))
+  }
+
+  useEffect( handleGetTasks, [] );
 
   return (
     <div className="app">
@@ -35,9 +52,39 @@ function App() {
       </Navbar>
 
       <Container>
+        {/* Subscribe form */}
+        <Row className="subscribe-form">
+          <Col sm={{ span: 6, offset: 3 }}>
+            <Card>
+              <Card.Body>
+                <p className="font-weight-bold">Enter phone number to receive sms message when a task gets completed.</p>
+                <SubscribeForm />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-        {/* Task header */}
+        {/* Add Task Title */}
+        <Row className="mt-4">
+          <Col sm={{ span: 6, offset: 3 }} className="text-center">
+            <h1>Add Task</h1>
+            <hr/>
+          </Col>
+        </Row>
+
+        {/* Add Task Form */}
         <Row>
+          <Col sm={{ span: 6, offset: 3 }}>
+            <Card>
+              <Card.Body>
+                <NewTaskForm reload={handleGetTasks}/>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        {/* Tasks Title */}
+        <Row className="mt-5">
           <Col sm={{ span: 6, offset: 3 }} className="text-center">
             <h1>Tasks</h1>
             <hr/>
@@ -52,19 +99,24 @@ function App() {
               {/* Itereate through eeach Task abject */}
               {tasks.map((task, idx) => {
                 return <Card key={task.id}>
-                  <Card.Header>
+                  <Card.Header className="d-flex justify-content-between align-items-center">
                     <Accordion.Toggle as={Button} variant="link" eventKey={idx}>
                       {task.title}
                     </Accordion.Toggle>
+
+                    <a href="#"><i className="fas fa-trash-alt" onClick={(e) => handleDeleteTask(e, task.id)} /></a>
+
                   </Card.Header>
                   <Accordion.Collapse eventKey={idx}>
                     <Card.Body>
 
                       {/* New Task Form Component */}
-                      {task.image ? null : <span><NewTaskForm taskId={task.id} /> <hr/></span> }
+                      { task.image ? null : <span><NewImageForm taskId={task.id} reload={handleGetTasks} /> <hr/></span> }
+
+                      <UpdateTaskAssignee assignee={task.assignee} taskId={task.id} reload={handleGetTasks} />
                       
                       {/* History Component */}
-                      <History history={task.history} image={task.image} />
+                      <History history={task.history} image={task.image} taskId={task.id} reload={handleGetTasks} />
 
                     </Card.Body>
                   </Accordion.Collapse>
@@ -80,10 +132,40 @@ function App() {
 }
 
 function History(props) {
+
+  function handleUpdateTask(event, taskId) {
+    event.preventDefault();
+
+    const payload = {
+      id: taskId
+    };
+
+    fetch(API, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(res => res.json())
+      .then(() => props.reload())
+      .catch(err => console.log(err))
+  }
+
   return (
     <div>
       <div className="text-center">
         {props.image ? <img className="uploaded-img" src={props.image} alt="Uploaded"/> : null }
+      </div>
+
+      <div>
+        <p className="font-weight-bold">Task Status 
+          <a href="#">
+            <span onClick={(e) => handleUpdateTask(e, props.taskId)} className="float-right badge badge-primary badge-pill">update</span>
+          </a>
+        </p>
+        
       </div>
       
       <ul>
